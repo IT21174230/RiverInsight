1d7e7e4f3be494e1da3474421db71e1d14c209f3
IT21174230 <it21174230@my.sliit.lk>
2025-02-12 14:51:20 +0530

    added navigation as a separate comp

---
+++ frontend_riverinsight/riverinsightfront/src/pages/Morphology.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MorphologicalPredictions() {
+  return (
+    <div>
+      <h1>Morphological Predictions</h1>
+      <p>Not a dashboard lol</p>
+    </div>
+  );
+}
+
+export default MorphologicalPredictions;
